cbe06d5ee29c3824ed876f14e52e92dbce0383a0
Usuario <Usuario@CPD02-M05>
2024-10-08 11:58:50 -0300

    Adicionar arquivos de projeto.

---
+++ projeto231224e231223/Views/frmCidades.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using projeto231224e231223.Models;
+
+namespace projeto231224e231223.Views
+{
+    public partial class frmCidades : Form
+    {
+        Cidade c;
+        public frmCidades()
+        {
+            InitializeComponent();
+        }
+        void limpaControles()
+        {
+            txtId.Clear();
+            txtNome.Clear();
+            txtUf.Clear();
+            txtPesquisar.Clear();
+        }
+        void carregarGrid(string pesquisa)
+        {
+            c = new Cidade()
+            {
+                nome = pesquisa
+            };
+
+            dgvCidades.DataSource = c.Consultar();
+        }
+
+
+        private void frmCidades_Load(object sender, EventArgs e)
+        {
+            limpaControles();
+            carregarGrid("");
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == string.Empty) return;
+
+            c = new Cidade()
+            {
+                nome = txtNome.Text,
+                uf = txtUf.Text
+            };
+            c.Incluir();
+
+            limpaControles();
+            carregarGrid("");
+        }
+
+        private void dgvCidades_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(dgvCidades.RowCount>0)
+            {
+                txtId.Text = dgvCidades.CurrentRow.Cells["id"].Value.ToString();
+                txtNome.Text = dgvCidades.CurrentRow.Cells["nome"].Value.ToString();
+                txtUf.Text = dgvCidades.CurrentRow.Cells["uf"].Value.ToString();
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text == string.Empty) return;
+
+            c = new Cidade()
+            {
+                id = int.Parse(txtId.Text),
+                nome = txtNome.Text,
+                uf = txtUf.Text
+            };
+            c.Alterar();
+
+            limpaControles();
+            carregarGrid("");
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text == "") return;
+
+            if (MessageBox.Show("Deseja excluir a cidade?", "Exclusão",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) 
+            {
+                c = new Cidade()
+                {
+                    id = int.Parse(txtId.Text)
+                };
+                c.Excluir();
+                limpaControles();
+                carregarGrid("");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpaControles();
+            carregarGrid("");
+
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            carregarGrid(txtPesquisar.Text);
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ projeto231224e231223/frmMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using projeto231224e231223.Views;
+
+namespace projeto231224e231223
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+            Banco.CriarBanco();
+        }
+
+        private void cidadesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCidades form = new frmCidades();
+            form.Show();
+        }
+    }
+}
